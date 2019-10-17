bb7fc1ce01a91e6e2cfa94c8614d7cee66a5d837
yuriy-burkut <burkut89@gmail.com>
2019-10-17 18:07:02 +0300

    Some code optimisation, added posibility to save state on orientation change in manifest file

--- src/main/java/com/yuriy/philipdickwiki/ScrollingActivity.kt
+++ src/main/java/com/yuriy/philipdickwiki/ScrollingActivity.kt
@@ -1,16 +1,11 @@
 package com.yuriy.philipdickwiki
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.ImageView
-import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_scrolling.*
-
-internal lateinit var scrollingText: TextView
-internal lateinit var toolbarImage: ImageView
+import kotlinx.android.synthetic.main.content_scrolling.*
 
 class ScrollingActivity : AppCompatActivity() {
 
@@ -18,10 +13,6 @@ class ScrollingActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_scrolling)
         setSupportActionBar(toolbar)
-
-        scrollingText = findViewById(R.id.text)
-        toolbarImage = findViewById(R.id.toolbar_image)
-
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -32,41 +23,39 @@ class ScrollingActivity : AppCompatActivity() {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
 
-            when (item.itemId) {
+        return when (item.itemId) {
             R.id.nav_home -> openHome()
             R.id.nav_biography -> openBiography()
             R.id.nav_books -> openBooks()
             R.id.nav_movies -> openMovies()
             else -> super.onOptionsItemSelected(item)
         }
-
-        return true
     }
 
-    private fun openHome() : Boolean {
+    private fun openHome(): Boolean {
         title = "Philip Kindred Dick"
-        scrollingText.text = getText(R.string.basic_info);
+        scrolling_text.text = getText(R.string.basic_info)
         toolbar_image.setImageResource(R.drawable.toolbar_image)
         return true
     }
 
-    private fun openBiography() : Boolean {
+    private fun openBiography(): Boolean {
         title = "Biography"
-        scrollingText.text = getText(R.string.biography);
+        scrolling_text.text = getText(R.string.biography)
         toolbar_image.setImageResource(R.drawable.toolbar_image_life)
         return true
     }
 
-    private fun openBooks() : Boolean {
+    private fun openBooks(): Boolean {
         title = "Books"
-        scrollingText.text = getText(R.string.books);
+        scrolling_text.text = getText(R.string.books)
         toolbar_image.setImageResource(R.drawable.toolbar_image_books)
         return true
     }
 
-    private fun openMovies() : Boolean {
+    private fun openMovies(): Boolean {
         title = "Movies"
-        scrollingText.text = getText(R.string.movies);
+        scrolling_text.text = getText(R.string.movies)
         toolbar_image.setImageResource(R.drawable.toolbar_image_movies)
         return true
     }
